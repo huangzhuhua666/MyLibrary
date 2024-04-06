63b4d7af981a278d2ec3974ab7c5a14b053e6612
hzh <453466831@qq.com>
2024-04-07 00:25:32 +0800

    feature. #web 创建web模块

---
+++ library-web/src/main/java/com/example/web/WebAgentManager.kt
@@ -0,0 +1,26 @@
+//package com.example.web
+//
+//import android.content.Context
+//import androidx.fragment.app.Fragment
+//
+///**
+// * Create by hzh on 2024/4/7.
+// */
+//object WebAgentManager {
+//
+//    fun init() {
+//
+//    }
+//
+//    fun getHybridFragment(): Fragment {
+//
+//    }
+//
+//    fun openWebPage(context: Context?) {
+//        context ?: return
+//    }
+//
+//    fun getWebViewController(context: Context?) {
+//        
+//    }
+//}
